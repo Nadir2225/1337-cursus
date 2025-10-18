3a358ad8d9cc24b5baabf7154919bee642d5d3ab
Nadir2225 <nadirna2003@gmail.com>
2025-10-18 10:16:16 +0100

    mandatory libft done

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,18 +6,18 @@
 /*   By: nel-ouad <nel-ouad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 17:02:45 by nel-ouad          #+#    #+#             */
-/*   Updated: 2025/10/17 18:15:24 by nel-ouad         ###   ########.fr       */
+/*   Updated: 2025/10/18 10:14:20 by nel-ouad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static int count_words(char const *s, char c)
+static int	count_words(char const *s, char c)
 {
 	int	count;
-	int i;
-	int in_word;
+	int	i;
+	int	in_word;
 
 	count = 0;
 	in_word = 0;
@@ -28,7 +28,8 @@ static int count_words(char const *s, char c)
 		{
 			count++;
 			in_word = 1;
-		} else if (in_word && s[i] == c)
+		}
+		else if (in_word && s[i] == c)
 		{
 			in_word = 0;
 		}
@@ -55,27 +56,15 @@ static char	*ft_strldup(const char *s, int i, int len)
 	return (dup);
 }
 
-char **ft_split(char const *s, char c)
+static void	fill_array(char const *s, char c, char	**words_array)
 {
-	char	**words_array;
-	int 	i;
-	int		in_word;
-	int		start;
-	int		l;
+	int	i;
+	int	start;
+	int	l;
 
 	i = 0;
 	l = 0;
-	in_word = 0;
-	if (!s)
-	{
-		words_array = malloc(sizeof(char *));
-		words_array[0] = NULL;
-		return (words_array);
-	}
-	words_array = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!words_array)
-		return (NULL);
-	while(s[i])
+	while (s[i])
 	{
 		if (s[i] != c)
 		{
@@ -87,6 +76,22 @@ char **ft_split(char const *s, char c)
 		else
 			i++;
 	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**words_array;
+
+	if (!s)
+	{
+		words_array = malloc(sizeof(char *));
+		words_array[0] = NULL;
+		return (words_array);
+	}
+	words_array = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!words_array)
+		return (NULL);
+	fill_array(s, c, words_array);
 	words_array[count_words(s, c)] = NULL;
 	return (words_array);
 }
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: nel-ouad <nel-ouad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 20:32:24 by nel-ouad          #+#    #+#             */
-/*   Updated: 2025/10/17 18:12:08 by nel-ouad         ###   ########.fr       */
+/*   Updated: 2025/10/18 10:15:39 by nel-ouad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,7 @@
 #include <stdlib.h>
 #include "libft.h" 
 
-static void print_split(char **result)
+int	main(void)
 {
-    int i = 0;
-    if (!result)
-    {
-        printf("Result is NULL\n");
-        return;
-    }
-    while (result[i])
-    {
-        printf("result[%d] = '%s'\n", i, result[i]);
-        i++;
-    }
-    printf("result[%d] = %s\n", i, result[i]); // should print (null)
-}
-
-int main(void)
-{
-    char **result;
-
-    printf("=== Test 1 ===\n");
-    result = ft_split("Hello world this is C", ' ');
-    print_split(result);
-    free(result);
-
-    printf("\n=== Test 2 ===\n");
-    result = ft_split("   split   these words  ", ' ');
-    print_split(result);
-    free(result);
-
-    printf("\n=== Test 3 ===\n");
-    result = ft_split("one,two,,three,", ',');
-    print_split(result);
-    free(result);
-
-    printf("\n=== Test 4 ===\n");
-    result = ft_split("", ' ');
-    print_split(result);
-    free(result);
-
-    printf("\n=== Test 5 ===\n");
-    result = ft_split(NULL, ' ');
-    print_split(result);
-    free(result);
-
-    return 0;
+	return (0);
 }
