55bc35d8be757cc115b329cf2c21af532ab37e4f
Nadir2225 <nadirna2003@gmail.com>
2026-01-20 15:34:20 +0100

    python_module 01

--- milestone_2/Python_Module_01/ex5/ft_plant_types.py
+++ milestone_2/Python_Module_01/ex5/ft_plant_types.py
@@ -13,22 +13,22 @@ class Flower(Plant):
         super().__init__(name, height, age)
         self._color = color
     
-    def bloom():
+    def bloom(self):
         print(f'{self._name} is blooming beautifully!')
     
-    def get_info():
-        print(f'{self.name} (Flower): {self._height}cm, {self._age} days, {self._color} color')
+    def get_info(self):
+        print(f'{self._name} (Flower): {self._height}cm, {self._age} days, {self._color} color')
 
 class Tree(Plant):
     def __init__(self, name: str, height: int, age: int, trunk_diameter: int) -> None:
         super().__init__(name, height, age)
         self._trunk_diameter = trunk_diameter
     
-    def produce_shade():
+    def produce_shade(self):
         print(f'{self._name} provides 78 square meters of shade')
 
-    def get_info():
-        print(f'{self.name} (Tree): {self._height}cm, {self._age} days, {self._trunk_diameter}cm diameter')
+    def get_info(self):
+        print(f'{self._name} (Tree): {self._height}cm, {self._age} days, {self._trunk_diameter}cm diameter')
 
 class Vegetable(Plant):
     def __init__(self, name: str, height: int, age: int, harvest_season: str, nutritional_value: str) -> None:
@@ -36,6 +36,20 @@ class Vegetable(Plant):
         self._harvest_season = harvest_season
         self._nutritional_value = nutritional_value
     
-    def get_info():
-        print(f'{self.name} (Vegetable): {self._height}cm, {self._age} days, {self.summer} harvest')
-    
+    def get_info(self):
+        print(f'{self._name} (Vegetable): {self._height}cm, {self._age} days, {self._harvest_season} harvest')
+
+print('=== Garden Plant Types ===\n')
+
+rose = Flower('Rose', 25, 30, 'red')
+oak = Tree('Oak', 500, 1825, 50)
+tomato = Vegetable('Tomato', 80, 90, 'summer', 'Tomato is rich in vitamin C')
+
+rose.get_info()
+rose.bloom()
+print()
+oak.get_info()
+oak.produce_shade()
+print()
+tomato.get_info()
+print(tomato._nutritional_value)
