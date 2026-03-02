ebbe1210c5d22d67b28cada293ee99a78ac4dfbe
Nadir El Ouadghiri <nel-ouad@c1r8p9.1337.ma>
2026-03-02 19:48:04 +0000

    ex0 in py 05

---
+++ milestone_2/Python_Module_05/ex1/data_stream.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import Any, List, Dict, Union, Optional
+
+class DataStream(ABC):
+	def __init__(self, stream_id: str):
+		self.stream_id = stream_id
+
+	@abstractmethod
+	def process_batch(self, data_batch: List[Any]) -> str:
+		pass
+
+	@abstractmethod
+	def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
+		pass
+
+	@abstractmethod
+	def get_stats(self) -> Dict[str, Union[str, int, float]]:
+		pass
