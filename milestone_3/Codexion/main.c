5c596819335be724bee099638dccbc8edc2548b4
Nadir2225 <nadirna2003@gmail.com>
2026-04-29 13:54:23 +0100

    1% done

--- milestone_3/Codexion/main.c
+++ milestone_3/Codexion/main.c
@@ -1,22 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/26 13:43:31 by marvin            #+#    #+#             */
+/*   Updated: 2026/04/26 13:43:31 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void validate_args(char **av)
 {
-	printf("%d", atoi("20JH5"));
 	if (0)
 		printf("%s", *av);
+	for (int i = 1; i < 8; i++)
+	{
+		int arg = atoi(av[i]);
+		if (arg <= 0)
+		{
+			printf("argument %d is not a positive integer\n", i);
+			exit(1);
+		}
+	}
+	if (strcmp(av[8], "fifo") != 0 && strcmp(av[8], "edf") != 0)
+	{
+		printf("argument 8 must be either 'fifo' or 'edf'\n");
+		exit(1);
+	}
 }
 
 int main(int ac, char **av)
 {
-	if (ac != 8)
+	if (ac != 9)
 	{
 		printf("wrong number of arguments please respect the following format:\n");
 		printf("<program_name> <number_of_coders> <time_to_burnout> ");
 		printf("<time_to_compile> <time_to_debug> <time_to_refactor> ");
-		printf("number_of_compiles_required> <dongle_cooldown>\n");
-		return (1);
+		printf("number_of_compiles_required> <dongle_cooldown> <scheduler>\n");
+		exit(1);
 	}
 	validate_args(av);
 	return (0);
